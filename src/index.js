c297ff3869c8f172019e751168ab7bbf7f3af989
Shady Israel <drsheko91@gmail.com>
2022-04-16 14:48:03 -0500

    edit html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./css/style.css"
 
 
-const task = function(title, description, dueDate,priority){
+const task = function(title, description, dueDate,priority,type){
     this.title = title ;
     this.description = description;
     this.dueDate= dueDate ;
